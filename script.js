498c2d2e344db2db07af4cde365784e85f2aa72a
Devon Vendetti <v3nd3tti@icloud.com>
2022-03-29 01:41:31 -0700

    first commit for CWsite. just uploading to link client to page for viewing.

---
+++ script.js
@@ -0,0 +1,40 @@
+$(document).ready(function() {
+  $(window).scroll(function() {
+    if (this.scrollY > 20) {
+      $('.navbar').addClass("sticky");
+    } else {
+      $('.navbar').removeClass("sticky");
+    }
+    if (this.scrollY > 500) {
+      $('.scroll-up-btn').addClass("show");
+    } else {
+      $('.scroll-up-btn').removeClass("show");
+    }
+  });
+
+  // SCROLL TO TOP
+  $('.scroll-up-btn').click(function() {
+    $('html').animate({scrollTop: 0});
+  });
+
+  // TOGGLE MENU - NAVBAR
+  $('.menu-btn').click(function() {
+    $('.navbar .menu').toggleClass('active');
+    $('.menu-btn i').toggleClass('active');
+  });
+
+  // TYPING ANIMATION SCRIPT
+  let typed1 = new Typed('.typed-1', {
+    strings: ["Model", "Blackjack Dealer", "BossBabe"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+  });
+
+  let typed2 = new Typed('.typed-2', {
+    strings: ["Model", "Blackjack Dealer", "BossBabe"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+  });
+});
